dcc7d972b287a8e0d740c56a8ae903b66113eb1e
gsdionis <36293931+gsdionis@users.noreply.github.com>
2018-03-15 20:01:34 +0100

    Update SantanderModelPostProcessingConfiguration.java

--- src/main/java/com/santander/wf/poc/postprocessing/SantanderModelPostProcessingConfiguration.java
+++ src/main/java/com/santander/wf/poc/postprocessing/SantanderModelPostProcessingConfiguration.java
@@ -3,6 +3,8 @@ package com.santander.wf.poc.postprocessing;
 import java.util.Arrays;
 import java.util.List;
 
+import com.santander.wf.poc.postprocessing.postprocGSD.DateProcessor;
+import com.santander.wf.poc.postprocessing.postprocGSD.NumbersProcessor;
 import com.workfusion.vds.api.nlp.processing.Processor;
 import com.workfusion.vds.api.nlp.processing.ProcessorsConfiguration;
 
@@ -10,7 +12,8 @@ public class SantanderModelPostProcessingConfiguration implements ProcessorsConf
 
     @Override
     public List<Processor> getProcessors() {
-        return Arrays.asList(new PostProcessorExample(),new LegGroupingProcessor());
+        return Arrays.asList(new PostProcessorExample(), new LegGroupingProcessor(), new DateProcessor(), new NumbersProcessor());
     }
+
 
 }
