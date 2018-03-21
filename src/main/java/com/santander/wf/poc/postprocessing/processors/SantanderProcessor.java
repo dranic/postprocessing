f0bda180711d61bfc50293801d57c2ecfe503857
gsdionis <36293931+gsdionis@users.noreply.github.com>
2018-03-21 13:01:16 +0100

    Update SantanderProcessor.java

--- src/main/java/com/santander/wf/poc/postprocessing/processors/SantanderProcessor.java
+++ src/main/java/com/santander/wf/poc/postprocessing/processors/SantanderProcessor.java
@@ -5,11 +5,13 @@ import java.util.Collection;
 
 import com.santander.wf.poc.postprocessing.normolization.AmountNormalizer;
 import com.santander.wf.poc.postprocessing.normolization.CaseNormalizer;
+import com.santander.wf.poc.postprocessing.normolization.TipoBarreraNormalizer;
 import com.workfusion.vds.api.nlp.model.Field;
 import com.workfusion.vds.api.nlp.model.IeDocument;
 import com.workfusion.vds.api.nlp.normalization.Normalizer;
 import com.workfusion.vds.api.nlp.processing.IeProcessor;
 import com.workfusion.vds.api.nlp.processing.ProcessingException;
+import com.workfusion.vds.nlp.processing.normalization.OcrAmountNormalizer;
 import com.workfusion.vds.nlp.processing.normalization.OcrDateNormalizer;
 import com.santander.wf.poc.postprocessing.normolization.PrecioNormalizer;
 
@@ -30,7 +32,9 @@ public class SantanderProcessor implements IeProcessor {
     public void process(IeDocument document) throws ProcessingException {
         processFields(document, new CaseNormalizer(), "opcion1", "opcion2","divisa1","divisa2");
         processFields(document, new OcrDateNormalizer("MM/dd/yyyy"), "fecha_pago_prima", "fecha_inicio", "fecha_vencimiento", "fecha_pago_vencimiento");
-        processFields(document, new AmountNormalizer(), "nominal1", "nominal2", "prima");
-        processFields(document, new PrecioNormalizer(), "precio","strike_");
+        processFields(document, new AmountNormalizer(), "nominal1", "nominal2", "prima","strike_", "barrier");
+        processFields(document, new OcrAmountNormalizer(),"nominal1", "nominal2", "prima","strike_", "barrier");
+        processFields(document, new PrecioNormalizer(), "precio");
+        processFields(document, new TipoBarreraNormalizer(), "tipo_barrera");
     }
 }
